0d62ed590de0875e508a7f8ff2547f3946793ed6
Brynhild <cn.zergirl@gmail.com>
2015-11-15 21:55:12 -0500

    fix styles.

--- TodoList.java
+++ TodoList.java
@@ -1,44 +1,21 @@
 import java.util.*;
 
-public class TodoList{
-	
-	static String welcomStat = new String("Hi,Kerrigan,here is your to-do list:");
-	
-/*
-	void SetTodo(){
-		
-		todos.add("1....");
-		todos.add("2....");
-		todos.add("3....");
-}
+public class TodoList {
+  
+  static String welcomStat = "Hi,Kerrigan,here is your to-do list:";
 
-	void displayAll(){
-		System.out.println(welcomStat);
-		for (int item :)
+  public static void main(String[] args) {
+    List<String> todoList = new ArrayList<>();
+    todoList.add("1....");
+    todoList.add("2....");
+    todoList.add("3....");
+    todoList.add("4....");
+    todoList.add("5....");
 
-	}
-*/
+    System.out.println(welcomStat);
 
-
-	public static void main(String[] args){
-
-//		TodoList todo = new TodoList();
-//		todo.displayAll();
-
-		List<String> todoList = new ArrayList<>();
-		todoList.add("1....");
-		todoList.add("2....");
-		todoList.add("3....");
-		todoList.add("4....");
-		todoList.add("5....");
-
-		System.out.println(welcomStat);
-
-		for(int index=0; index<todoList.size(); index++){
-		System.out.println(todoList.get(index));
-	}
-
-
-	}
-
+    for(int index = 0; index < todoList.size(); index++) {
+      System.out.println(todoList.get(index));
+    }
+  }
 }
