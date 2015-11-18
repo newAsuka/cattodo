c92b2b00d7f7634439d3938432ba75e6384f4493
Brynhild <cn.zergirl@gmail.com>
2015-11-18 02:19:44 -0500

    Kerrigan solution

--- TodoList.java
+++ TodoList.java
@@ -1,22 +1,51 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TodoList {
   
   static String welcomStat = "Hi,Kerrigan,here is your to-do list:";
+  private Boolean status = true;
+  private ArrayList<String> todoList ;
 
+  public void TodoList () {
+    System.out.println(welcomStat);
+    List<String> todoList = new ArrayList<>(
+        Array.asList("1...","2...","3...","4...","5..."));
+    
+    for (String item : todoList) {
+      System.out.println(item);
+    }
+  
+  public void setStatus (int index) {
+    while (index>0 || index<todoList.size()) {
+        if todoList.[index-1].endWith("DONE") {
+          return;
+        }
+        else {
+          todoList.[index-1] = todoList.[index].concat(status.toString())
+        };
+        System.out.println("must between 1-5");
+        //waiting for input
+    }
+}
+
+  public void String getStatus(int index){
+
+  }
+
+
+
+  
+}
+
+public class markAsDone {
   public static void main(String[] args) {
 
-    List<String> todoList = Arrays.asList(
-    	"1...",
-    	"2...",
-    	"3...",
-    	"4...",
-    	"5...");
+    
+      
 
-    System.out.println(welcomStat);
 
-    for (String item : todoList) {
-    	System.out.println(item);
+    
     }
   }
 }
