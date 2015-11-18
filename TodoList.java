b398dfff102bb9a9cebed4ec2c653730efdb959b
Brynhild <cn.zergirl@gmail.com>
2015-11-18 02:20:46 -0500

    Fix condition checking.

--- TodoList.java
+++ TodoList.java
@@ -11,18 +11,24 @@ public class TodoList {
     System.out.println(welcomStat);
     List<String> todoList = new ArrayList<>(
         Array.asList("1...","2...","3...","4...","5..."));
-    
+
     for (String item : todoList) {
       System.out.println(item);
     }
+  }
   
-  public void setStatus (int index) {
-    while (index>0 || index<todoList.size()) {
-        if todoList.[index-1].endWith("DONE") {
+// 1. && and,  || or
+// 2. Correct condition - exit loop / incorrect condition - loop for input
+// 3. Off-by-one: [1, 5]  [1, 5): always use 0-based internally.
+
+  public void setStatus(int index) {
+    int i = index - 1; // [1, 5] -> [0, 5)
+    while (!(index>=0 && index<todoList.size())) {
+        if todoList.[index].endWith("DONE") {
           return;
         }
         else {
-          todoList.[index-1] = todoList.[index].concat(status.toString())
+          todoList.[index] = todoList.[index+1].concat(status.toString())
         };
         System.out.println("must between 1-5");
         //waiting for input
